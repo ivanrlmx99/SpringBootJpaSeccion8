9431d33c6edc42cf0d8562c2f4bae1859d7598a4
ivanrlmx99 <ivanlaureano77@gmail.com>
2020-10-13 13:46:04 -0500

    proyecto de jpa

---
+++ src/main/java/com/navy/mx/app/SpringBootMvcDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.navy.mx.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navy/mx/app/SpringBootMvcDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navy.mx.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

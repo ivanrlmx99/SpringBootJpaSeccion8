4095fe76a3dcc424f043496538afb4b6bf275112
ivanrlmx99 <ivanlaureano77@gmail.com>
2020-10-13 20:14:30 -0500

    se afrego la clase identidad

---
+++ src/main/java/com/navy/mx/app/models/dao/IClienteDao.java
@@ -0,0 +1,11 @@
+package com.navy.mx.app.models.dao;
+
+import java.util.List;
+
+import com.navy.mx.app.models.entity.Cliente;
+
+public interface IClienteDao {
+	
+	public List<Cliente>findAll();
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO CLIENTES(id,nombre,apellido,email,create_at) values(99,'Itzelth','Gonzalez','itza@gmail.com','2020-10-13');
